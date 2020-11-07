8d67713ff697a92e0606b16a0c60571d816b75f1
Adash Geethagovindan <ageethag@uncc.edu>
2020-11-07 17:38:40 -0500

    first cycle

--- TestInvoice.py
+++ TestInvoice.py
@@ -1,8 +1,11 @@
 import pytest
-
+from Invoice import Invoice
 @pytest.fixture()
 
 def products():
     products = {'Pen': {'qnt': 10, 'unit_price': 3.75, 'discount': 5},
                 'Notebook': {'qnt': 5, 'unit_price': 7.5, 'discount': 10}}
     return products
+
+def test_CanFindInvoiceClass():
+    invoice = Invoice()
