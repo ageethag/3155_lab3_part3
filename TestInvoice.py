f19f73eaa96c3eb442ca02ae84a6a1118b5f5329
Adash Geethagovindan <ageethag@uncc.edu>
2020-11-07 17:34:30 -0500

    first initial stage of files

---
+++ TestInvoice.py
@@ -0,0 +1,8 @@
+import pytest
+
+@pytest.fixture()
+
+def products():
+    products = {'Pen': {'qnt': 10, 'unit_price': 3.75, 'discount': 5},
+                'Notebook': {'qnt': 5, 'unit_price': 7.5, 'discount': 10}}
+    return products
